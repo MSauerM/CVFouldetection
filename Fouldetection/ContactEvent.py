8558b536b82b1f548f7201d41556f93e9f582ca2
Matthias Sauer <matthias.sauer97@gmx.net>
2021-04-29 22:18:53 +0200

    Restored most of previous code

--- Fouldetection/ContactEvent.py
+++ Fouldetection/ContactEvent.py
@@ -3,8 +3,15 @@ class ContactEvent:
     frameCountBegin = -1
     frameCountEnd = -1
     isFoul = False
+    # involved PlayerBoundingBox
+    # sequence which is the corresponding one to this event
 
     def __init__(self, frameCountBegin, frameCountEnd):
         self.frameCountBegin = frameCountBegin
         self.frameCountEnd = frameCountEnd
 
+    def __str__(self):
+        print("Here goes something with the information about a contact event")
+
+    def __repr__(self):
+        return self.__str__()
---
+++ Fouldetection/PlayerBoundingBox.py
@@ -0,0 +1,9 @@
+
+
+class PlayerBoundingBox:
+
+
+    def __init__(self, mainColor, boundingBox, frameCount):
+        self.mainColor = mainColor
+        self.boundingBox = boundingBox
+        self.frameCount = frameCount
