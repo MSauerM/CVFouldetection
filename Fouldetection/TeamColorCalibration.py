a98a8b33b657007aa06fd2e2dc704123fae290a8
Matthias Sauer <matthias.sauer97@gmx.net>
2021-05-14 18:19:03 +0200

    Added base implementation for TeamColorCalibration.py

---
+++ Fouldetection/TeamColorCalibration.py
@@ -0,0 +1,10 @@
+
+class TeamColorCalibration:
+
+    colors_list = []
+
+    def __init__(self):
+        print ("TeamColorCalibration")
+
+    def calibrate(self, img):
+        print ("Calibrate")
--- appconfig.py
+++ appconfig.py
@@ -1,2 +1,3 @@
 
 use_multithreading = False
+team_color_calib_every_frame = False
