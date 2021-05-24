35824602fe4e2b1c565c9e04ee5500beb3509297
Matthias Sauer <matthias.sauer97@gmx.net>
2021-05-24 16:44:54 +0200

    Implemented Color Calibration further

--- Fouldetection/ColorHistogram.py
+++ Fouldetection/ColorHistogram.py
@@ -1,10 +1,13 @@
 from cv2 import cv2 as cv
 from matplotlib import pyplot as plt
 from CVUtility import ImageUtility as utility
+from itertools import chain
+import numpy as np
 
 class ColorHistogram:
 
     _hist = None
+    _main_color_list = None
 
     def __init__(self, img, mask=None, bins=180, range=180):
         print("HSV Color Histogram")
@@ -22,6 +25,44 @@ class ColorHistogram:
         plt.xlim([0, 180])
         plt.show()
 
+    def _find_peaks(self, amount: int):
+        local_peaks = []
+        excluded_indices = []
+        mask = np.zeros(self._hist.size, dtype=bool)
+        histogram_copy = self._hist
+        for i in range(amount):
+            if excluded_indices:
+                mask[excluded_indices] = True
+                histogram_copy = np.ma.array(self._hist, mask=mask)
+            value = np.amax(histogram_copy)
+            index = np.where(histogram_copy == value)[0][0] # index corresponds to the hue
+            local_peaks.append([index, value])
+            lower_local_bound = index-15
+            upper_local_bound = index+15
+            local_range = None
+            if upper_local_bound > 179:
+                local_range = chain(range(lower_local_bound, 180), range(0,upper_local_bound-179))
+            else:
+                local_range = range(index-15, index +15)
+            excluded_indices.extend(local_range)
+        return local_peaks
 
-    def get_main_colors(self):
-        return None
+    def get_main_colors(self, amount: int):
+        if self._main_color_list is None:
+            #print(self._hist[0])
+            self._main_color_list = []
+
+            local_peaks = self._find_peaks(amount)
+            local_peaks.sort(key=lambda x:x[1], reverse=True) # Theoretically unnecessary, because local peaks are always found from big to small
+            self._main_color_list = local_peaks
+            #for value in self._hist:
+            #    if not self._main_color_list or len(self._main_color_list) < amount:
+            #        self._main_color_list.append([np.where(self._hist == value)[0], value])
+            #        self._main_color_list.sort(key = lambda x:x[1], reverse=True) # sorting by the second value which is the pixelamount
+            #    else:
+            #        if value > self._main_color_list[-1][1]:
+            #            index = np.where(self._hist == value)[0]
+            #            self._main_color_list.append([index, value])
+            #            self._main_color_list.sort(key=lambda x: x[1], reverse=True)
+            #            del self._main_color_list[-1]
+        return self._main_color_list
--- Fouldetection/TeamColorCalibration.py
+++ Fouldetection/TeamColorCalibration.py
@@ -1,4 +1,5 @@
-import cv2
+import cv2 as cv
+import numpy as np
 
 from Fouldetection.ColorHistogram import ColorHistogram
 from CVUtility import ImageUtility as utility
@@ -19,9 +20,22 @@ class TeamColorCalibration:
 
 
         # 1
-
+        histogram_colors = histogram.get_main_colors(5)
         histogram.show_histogram()
+        first_color = histogram_colors[0]
+        second_color = histogram_colors[1]
+
+        self.colors_list.append(first_color)
+        self.colors_list.append(second_color)
+
+        img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        image_one = cv.inRange(img_hsv, np.array([first_color[0]-20, 100, 100]), np.array([first_color[0]+20, 255, 255]))
+        image_two = cv.inRange(img_hsv, np.array([second_color[0]-20, 100, 100]), np.array([second_color[0]+20, 255, 255]))
+
+        utility.showResizedImage("TeamColorCalibration - Image One ", image_one, 0.4)
+        utility.showResizedImage("TeamColorCalibration - Image Two ", image_two, 0.4)
+
         utility.showResizedImage("TeamColorCalibration - Image", img, 0.4)
         utility.showResizedImage("TeamColorCalibration - Mask", mask, 0.4)
-        utility.showResizedImage("TeamColorCalibration - Fusion", cv2.bitwise_and(img, img, mask= mask), 0.4)
+        utility.showResizedImage("TeamColorCalibration - Fusion", cv.bitwise_and(img, img, mask= mask), 0.4)
         self.isCalibrated = True
