0b085bc6e725779072727de9decd216c97799b7f
Matthias Sauer <matthias.sauer97@gmx.net>
2021-05-25 11:02:59 +0200

    Implemented base version of PerformanceEvaluator.py

---
+++ CVUtility/PerformanceEvaluator.py
@@ -0,0 +1,11 @@
+
+class PerformanceEvaluator:
+
+    def start_evaluation(self):
+        print("start of evaluation")
+
+    def end_evaluation(self):
+        print("End of evaluation")
+
+    def evaluate_time(self):
+        print("Time")
