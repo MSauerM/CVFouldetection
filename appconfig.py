a6c6d6b16d2b60d8c95c19ef06251a18370eed5a
Matthias Sauer <matthias.sauer97@gmx.net>
2021-05-31 21:42:01 +0200

    Implemented cropping strategy, needs further testing

--- appconfig.py
+++ appconfig.py
@@ -1,5 +1,12 @@
+from enum import Enum
+
+class CroppingStrategy(Enum):
+    STATIONARY = 1
+    DYNAMIC_FIXED = 2
+    DYNAMIC_VARIED = 3
 
 use_multithreading = False
 team_color_calib_every_frame = False
 max_frame_amount = 2000
 show_debug_windows = True
+cropping_strategy = CroppingStrategy.DYNAMIC_VARIED
