d92b08769b6f553dcb0b112d09251cd771dc2611
Matthias Sauer <matthias.sauer97@gmx.net>
2021-07-30 23:25:36 +0200

    Implemented min area rect box for human pose estimation

--- Fouldetection/Analyzers/FoulAnalyzer.py
+++ Fouldetection/Analyzers/FoulAnalyzer.py
@@ -1,7 +1,9 @@
+import cv2
+
 import appconfig
 from Fouldetection.Analyzers.ActionRecognizer import ActionRecognizer
 import numpy as np
-
+from cv2 import cv2 as cv
 
 class FoulAnalyzer:
 
@@ -17,6 +19,7 @@ class FoulAnalyzer:
         print(joints)
         # change nd array to normal array
         distance_matrix_list = []
+        angle_skeleton_list = []
         for key in joints:
             # print(joints[key][0]) Skelettebene
             # joints[key][1] confidences für jeweiliges Skelett
@@ -26,11 +29,20 @@ class FoulAnalyzer:
                 # Berechnung des Nähekoeffizienten
 
                 for z in range(0, joints[key][0].shape[0]-1):
+                    box = cv.minAreaRect(joints[key][0][z])
+                    box_points = cv.boxPoints(box)
+                    box_angle = box[2]
+                    box_points = np.int0(box_points)
+                    angle_skeleton_list.append([z, box_angle, box[0], box_points])
                     for d in range (1, joints[key][0].shape[0]):
                         #skelett 1
                         skeleton1 = joints[key][0][z]
                         #skelett 2
                         skeleton2 = joints[key][0][z+d]
+                        #box2 = cv.minAreaRect(skeleton2)
+                        #box2_angle = box2[2]
+                        #box2_points = cv.boxPoints(box2)
+                        #box2_points = np.int0(box2_points)
                         distance = 0.0
                         distance_counter = 0
                         for a in skeleton1:
@@ -46,8 +58,15 @@ class FoulAnalyzer:
                 #for i in range(z, joints[key][0].shape[1]):
                 distance_matrix_list.append(distance_matrix)
             else:
-                distance_matrix_list.append(None)
+                distance_matrix_list.append(None) # evtl. []
+                angle_skeleton_list.append(None) # evtl. []
             print("Test")
+
+        # Verketten der Skeletons
+
+        # Testen ob bei diesem Skeletons eine Winkeländerung größer als Threshold auftritt
+
+
 
     def analyze_action(self, sequence):
         print("Analyze Foul based on Action Recognition")
