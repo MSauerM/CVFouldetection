2146f509f8a9ba05a7da55af56bb902a9ec74080
Matthias Sauer <matthias.sauer97@gmx.net>
2021-08-18 10:17:51 +0200

    Preparation for action recognition and human pose estimation evaluation

---
+++ evaluation_action_recognition.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_001 Dortmund - Schalke.mp4' -v 'Foul_001_are' -o 'Foul_001_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_002 Dortmund - Schalke.mp4' -v 'Foul_002_are' -o 'Foul_002_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_003 Dortmund - Ingolstadt.mp4' -v 'Foul_003_are' -o 'Foul_003_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_004 Dortmund - Ingolstadt.mp4' -v 'Foul_004_are' -o 'Foul_004_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_005 Dortmund - Ingolstadt.mp4' -v 'Foul_005_are' -o 'Foul_005_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_006 Dortmund - Ingolstadt.mp4' -v 'Foul_006_are' -o 'Foul_006_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_007 Dortmund - Frankfurt.mp4' -v 'Foul_007_are' -o 'Foul_007_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_008 Dortmund - Frankfurt.mp4' -v 'Foul_008_are' -o 'Foul_008_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_009 Dortmund - Frankfurt.mp4' -v 'Foul_009_are' -o 'Foul_009_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_010 Dortmund - Frankfurt.mp4' -v 'Foul_010_are' -o 'Foul_010_are' --action_recognition True
+
+python main.py -f ../Evaluationsszenen/No_Fouls/'Clear_No_Foul_001 Dortmund - Schalke.mp4' -v 'No_Foul_001_are' -o 'No_Foul_001_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/No_Fouls/'Clear_No_Foul_002 Dortmund - Schalke.mp4' -v 'No_Foul_002_are' -o 'No_Foul_002_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/No_Fouls/'Clear_No_Foul_003 Dortmund - Ingolstadt.mp4' -v 'No_Foul_003_are' -o 'No_Foul_003_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/No_Fouls/'Clear_No_Foul_004 Dortmund - Ingolstadt.mp4' -v 'No_Foul_004_are' -o 'No_Foul_004_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/No_Fouls/'Clear_No_Foul_005 Dortmund - Frankfurt.mp4' -v 'No_Foul_005_are' -o 'No_Foul_005_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/No_Fouls/'No_Foul_001 Schwalbe Dortmund - Schalke.mp4' -v 'No_Foul_S_001_are' -o 'No_Foul_S_001_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/No_Fouls/'No_Foul_002 Schwalbe Dortmund - Ingolstadt.mp4' -v 'No_Foul_S_002_are' -o 'No_Foul_S_002_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/No_Fouls/'No_Foul_003 Schwalbe Dortmund - Frankfurt.mp4' -v 'No_Foul_S_003_are' -o 'No_Foul_S_003_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/No_Fouls/'No_Foul_004 Schwalbe Dortmund - Frankfurt.mp4' -v 'No_Foul_S_004_are' -o 'No_Foul_S_004_are' --action_recognition True
+python main.py -f ../Evaluationsszenen/No_Fouls/'No_Foul_005 Schwalbe Dortmund - Frankfurt.mp4' -v 'No_Foul_S_005_are' -o 'No_Foul_S_005_are' --action_recognition True
---
+++ evaluation_hpe.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_001 Dortmund - Schalke.mp4' -v 'Foul_001_hpe' -o 'Foul_001_hpe' --human_pose_estimation True 
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_002 Dortmund - Schalke.mp4' -v 'Foul_002_hpe' -o 'Foul_002_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_003 Dortmund - Ingolstadt.mp4' -v 'Foul_003_hpe' -o 'Foul_003_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_004 Dortmund - Ingolstadt.mp4' -v 'Foul_004_hpe' -o 'Foul_004_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_005 Dortmund - Ingolstadt.mp4' -v 'Foul_005_hpe' -o 'Foul_005_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_006 Dortmund - Ingolstadt.mp4' -v 'Foul_006_hpe' -o 'Foul_006_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_007 Dortmund - Frankfurt.mp4' -v 'Foul_007_hpe' -o 'Foul_007_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_008 Dortmund - Frankfurt.mp4' -v 'Foul_008_hpe' -o 'Foul_008_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_009 Dortmund - Frankfurt.mp4' -v 'Foul_009_hpe' -o 'Foul_009_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/Fouls/'Foul_010 Dortmund - Frankfurt.mp4' -v 'Foul_010_hpe' -o 'Foul_010_hpe' --human_pose_estimation True
+
+python main.py -f ../Evaluationsszenen/No_Fouls/'Clear_No_Foul_001 Dortmund - Schalke.mp4' -v 'No_Foul_001_hpe' -o 'No_Foul_001_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/No_Fouls/'Clear_No_Foul_002 Dortmund - Schalke.mp4' -v 'No_Foul_002_hpe' -o 'No_Foul_002_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/No_Fouls/'Clear_No_Foul_003 Dortmund - Ingolstadt.mp4' -v 'No_Foul_003_hpe' -o 'No_Foul_003_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/No_Fouls/'Clear_No_Foul_004 Dortmund - Ingolstadt.mp4' -v 'No_Foul_004_hpe' -o 'No_Foul_004_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/No_Fouls/'Clear_No_Foul_005 Dortmund - Frankfurt.mp4' -v 'No_Foul_005_hpe' -o 'No_Foul_005_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/No_Fouls/'No_Foul_001 Schwalbe Dortmund - Schalke.mp4' -v 'No_Foul_S_001_hpe' -o 'No_Foul_S_001_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/No_Fouls/'No_Foul_002 Schwalbe Dortmund - Ingolstadt.mp4' -v 'No_Foul_S_002_hpe' -o 'No_Foul_S_002_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/No_Fouls/'No_Foul_003 Schwalbe Dortmund - Frankfurt.mp4' -v 'No_Foul_S_003_hpe' -o 'No_Foul_S_003_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/No_Fouls/'No_Foul_004 Schwalbe Dortmund - Frankfurt.mp4' -v 'No_Foul_S_004_hpe' -o 'No_Foul_S_004_hpe' --human_pose_estimation True
+python main.py -f ../Evaluationsszenen/No_Fouls/'No_Foul_005 Schwalbe Dortmund - Frankfurt.mp4' -v 'No_Foul_S_005_hpe' -o 'No_Foul_S_005_hpe' --human_pose_estimation True
